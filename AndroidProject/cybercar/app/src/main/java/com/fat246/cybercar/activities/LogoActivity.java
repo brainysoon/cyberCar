b9758802d3bd01deba5aa7af506ac1fa597b808f
fa246 <sunxcchn918@163.com>
2016-03-28 21:51:51 +0800

    add-logo-activity
    添加  logo 页面但是还没有写完，今天先告一段路。:(
    还有一个写bug没有解决。

---
+++ AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/activities/LogoActivity.java
@@ -0,0 +1,15 @@
+package com.fat246.cybercar.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fat246.cybercar.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
