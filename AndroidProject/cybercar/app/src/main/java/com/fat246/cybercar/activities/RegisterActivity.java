ebc9fdfa1213e237c09c3423b0fdd98eb21441c7
fa246 <sunxcchn918@163.com>
2016-04-01 19:40:25 +0800

    add-something-donot-know

--- AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/activities/RegisterActivity.java
+++ AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/activities/RegisterActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.fat246.cybercar.R;
 import com.fat246.cybercar.fragments.RegisterBasicInfoFragment;
-import com.fat246.cybercar.fragments.RegisterDetailInfoFragment;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +15,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         Fragment mFragment=new RegisterBasicInfoFragment();
+        
         //Load Fragmentw
         getSupportFragmentManager().beginTransaction().
                 add(R.id.activity_register_framelayout,mFragment).commit();
