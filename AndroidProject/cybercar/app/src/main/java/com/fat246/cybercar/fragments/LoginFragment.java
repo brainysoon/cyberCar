39060f81107d014b142b4778495de0506378384a
fa246 <sunxcchn918@163.com>
2016-04-02 21:34:35 +0800

    add-some-updates
    ...

--- AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/fragments/LoginFragment.java
+++ AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/fragments/LoginFragment.java
@@ -1,9 +1,45 @@
 package com.fat246.cybercar.fragments;
 
-import android.support.v4.app.Fragment;
 
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fat246.cybercar.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class LoginFragment extends Fragment {
 
-    //可获得用户信息接口
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
 
 }
--- AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/fragments/LogoFragment.java
+++ AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/fragments/LogoFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.fat246.cybercar.R;
@@ -26,8 +27,39 @@ public class LogoFragment extends Fragment {
 
         View rootView=inflater.inflate(R.layout.fragment_logo,container,false);
         mImageView=(ImageView)rootView.findViewById(R.id.fragment_logo_imageview);
+        setRootView(rootView);
 
         return rootView;
+    }
+
+    private void setRootView(View rootView){
+
+        Button buttonLogin=(Button)rootView.findViewById(R.id.fragment_logo_button_login);
+        Button buttonRegister=(Button)rootView.findViewById(R.id.fragment_logo_button_register);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mIntent=new Intent(getActivity(),LoginActivity.class);
+
+                startActivity(mIntent);
+
+                getActivity().finish();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mIntent=new Intent(getActivity(),RegisterActivity.class);
+
+                startActivity(mIntent);
+
+                getActivity().finish();
+            }
+        });
     }
 
     @Override
@@ -35,22 +67,22 @@ public class LogoFragment extends Fragment {
         super.onResume();
 
         //设置透明度为
-        mImageView.setAlpha(0.3f);
+//        mImageView.setAlpha(0.3f);
 
-        //设置动画 并且在动画结束的时候跳转到登陆界面
-        mImageView.animate()
-                .alpha(1.0f)
-                .setDuration(1000)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-
-                        Intent mIntent=new Intent(getActivity(), RegisterActivity.class);
-
-                        startActivity(mIntent);
-
-                        getActivity().finish();
-                    }
-                });
+//        //设置动画 并且在动画结束的时候跳转到登陆界面
+//        mImageView.animate()
+//                .alpha(1.0f)
+//                .setDuration(1000)
+//                .setListener(new AnimatorListenerAdapter() {
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+//
+//                        Intent mIntent=new Intent(getActivity(), RegisterActivity.class);
+//
+//                        startActivity(mIntent);
+//
+//                        getActivity().finish();
+//                    }
+//                });
     }
 }
