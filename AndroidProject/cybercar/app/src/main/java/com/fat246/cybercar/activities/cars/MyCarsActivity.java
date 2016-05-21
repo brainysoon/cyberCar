24f03d59178f6e5d5aa3a8866011e5f768982ca0
ken <kensoon918@163.com>
2016-05-21 20:58:45 +0800

    add-display-my-cars
    显示我的汽车的模块做好了，嘿嘿。

--- AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/activities/cars/MyCarsActivity.java
+++ AndroidProject/cybercar/app/src/main/java/com/fat246/cybercar/activities/cars/MyCarsActivity.java
@@ -1,13 +1,56 @@
 package com.fat246.cybercar.activities.cars;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fat246.cybercar.R;
+import com.fat246.cybercar.application.MyApplication;
+import com.fat246.cybercar.beans.Brand;
+import com.fat246.cybercar.beans.Car;
+import com.fat246.cybercar.beans.Model;
+import com.fat246.cybercar.openwidgets.CircleImageView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.DownloadFileListener;
+import cn.bmob.v3.listener.FindListener;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 public class MyCarsActivity extends AppCompatActivity {
+
+    private PtrClassicFrameLayout mPtrFrame;
+    private ListView mListView;
+    private FloatingActionButton mAction;
+
+    //data
+    private List<Car> mCarData = new ArrayList<>();
+    private HashMap<String, Model> mModelData = new LinkedHashMap<>();
+    private HashMap<String, Brand> mBrandData = new LinkedHashMap<>();
+
+    private CarsAdapter mAdapter;
+
+    //Listener
+    private ModelFindListener mModelListener = new ModelFindListener();
+    private BrandFindListener mBrandListener = new BrandFindListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +59,54 @@ public class MyCarsActivity extends AppCompatActivity {
 
         initToolbar();
 
+        initView();
+
+        mPtrFrame.autoRefresh();
+    }
+
+    //initView;
+    private void initView() {
+
+        mPtrFrame = (PtrClassicFrameLayout) findViewById(R.id.activity_my_cars_ptr);
+        mListView = (ListView) findViewById(R.id.activity_my_cars_list);
+        mAction = (FloatingActionButton) findViewById(R.id.activity_my_cars_action);
+
+        initListView();
+
+        initPtr();
+
+        beginToRefresh();
+    }
+
+    //initPtr
+    private void initPtr() {
+
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+
+                //开始刷新
+                beginToRefresh();
+
+                mPtrFrame.refreshComplete();
+            }
+        });
+
+    }
+
+    private void initListView() {
+
+        mAdapter = new CarsAdapter(MyCarsActivity.this);
+
+        mListView.setAdapter(mAdapter);
     }
 
     //initToolbar
@@ -36,6 +127,173 @@ public class MyCarsActivity extends AppCompatActivity {
                     MyCarsActivity.this.finish();
                 }
             });
+        }
+    }
+
+    //
+    class CarsAdapter extends BaseAdapter {
+
+        private Context context;
+        private LayoutInflater layoutInflater;
+
+        public CarsAdapter(Context context) {
+
+            this.context = context;
+
+            layoutInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mCarData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mCarData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            convertView = layoutInflater.inflate(R.layout.activity_my_cars_item, null);
+
+            initView(convertView, position);
+
+            return convertView;
+        }
+
+        //initView
+        private void initView(View view, int i) {
+
+            final CircleImageView mAvatorView = (CircleImageView) view.findViewById(R.id.activity_my_cars_image);
+            TextView mNum = (TextView) view.findViewById(R.id.activity_my_cars_item_num);
+            TextView mNick = (TextView) view.findViewById(R.id.activity_my_cars_item_nick);
+
+            Car mCar = mCarData.get(i);
+
+            Model mModel = mModelData.get(mCar.getCar_ModelType());
+
+            if (mModel != null) {
+
+                Brand mBrand = mBrandData.get(mModel.getBrand_Name());
+
+                if (mBrand != null) {
+
+                    mBrand.getBrand_Sign().download(MyCarsActivity.this, new DownloadFileListener() {
+                        @Override
+                        public void onSuccess(String s) {
+
+                            Bitmap bt = BitmapFactory.decodeFile(s);//图片地址
+
+                            mAvatorView.setImageBitmap(bt);
+                        }
+
+                        @Override
+                        public void onFailure(int i, String s) {
+
+                        }
+                    });
+                }
+            }
+
+            mNum.setText(mCar.getCar_Num());
+
+            mNick.setText(mCar.getCar_Nick());
+        }
+    }
+
+    private void beginToRefresh() {
+
+        final BmobQuery<Car> query = new BmobQuery<>("Car");
+
+        query.addWhereMatches("User_Tel", MyApplication.mUser.getUser_Tel());
+
+        query.findObjects(MyCarsActivity.this, new FindListener<Car>() {
+            @Override
+            public void onSuccess(List<Car> list) {
+
+                if (list.size() > 0) {
+
+                    mCarData = list;
+
+                    mModelData.clear();
+                    mBrandData.clear();
+
+                    for (Car i : list) {
+
+                        //没有这个型号
+                        if (mModelData.get(i.getCar_ModelType()) == null) {
+
+                            BmobQuery<Model> query1 = new BmobQuery<>("Model");
+
+                            query1.addWhereMatches("Model_Name", i.getCar_ModelType());
+
+                            query1.findObjects(MyCarsActivity.this, MyCarsActivity.this.mModelListener);
+                        }
+                    }
+                }
+
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Toast.makeText(MyCarsActivity.this, "加载失败！", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    class ModelFindListener extends FindListener<Model> {
+
+        @Override
+        public void onSuccess(List<Model> list) {
+
+            if (list.size() > 0) {
+
+                Model model = list.get(0);
+                mModelData.put(model.getModel_Name(), model);
+
+                if (mBrandData.get(model.getBrand_Name()) == null) {
+
+                    BmobQuery<Brand> query2 = new BmobQuery<>("Brand");
+
+                    query2.addWhereMatches("Brand_Name", model.getBrand_Name());
+
+                    query2.findObjects(MyCarsActivity.this, MyCarsActivity.this.mBrandListener);
+                }
+            }
+        }
+
+        @Override
+        public void onError(int i, String s) {
+
+        }
+    }
+
+    class BrandFindListener extends FindListener<Brand> {
+
+        @Override
+        public void onSuccess(List<Brand> list) {
+
+            if (list.size() > 0) {
+
+                Brand brand = list.get(0);
+
+                mBrandData.put(brand.getBrand_Name(), brand);
+            }
+        }
+
+        @Override
+        public void onError(int i, String s) {
+
         }
     }
 }
