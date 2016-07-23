807277b9fc3eeb4118010b5c6729d28031aec190
kensoon <kensoon918@163.com>
2016-07-23 16:11:42 +0800

    add-logo-activity-and-my-application

---
+++ AndroidProject/gasService/app/src/main/java/com/fat246/gasservice/MyApplication.java
@@ -0,0 +1,14 @@
+package com.fat246.gasservice;
+
+import android.app.Application;
+
+/**
+ * Created by ken on 16-7-23.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
