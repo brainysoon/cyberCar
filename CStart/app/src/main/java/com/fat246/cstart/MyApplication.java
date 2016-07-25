be237f9c12fd55d9324da91176a00a6716e74703
kensoon <kensoon918@163.com>
2016-07-25 11:03:05 +0800

    add-my-application-file

---
+++ CStart/app/src/main/java/com/fat246/cstart/MyApplication.java
@@ -0,0 +1,13 @@
+package com.fat246.cstart;
+
+import android.app.Application;
+
+/**
+ * Created by ken on 16-7-25.
+ */
+public class MyApplication extends Application {
+
+    public MyApplication() {
+        super();
+    }
+}
