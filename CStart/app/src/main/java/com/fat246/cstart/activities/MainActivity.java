7ed586a359fc5c8b0201c6dc06a15c5a6f6a0d20
kensoon <kensoon918@163.com>
2016-07-26 18:33:51 +0800

    seo-main-activity

--- CStart/app/src/main/java/com/fat246/cstart/activities/MainActivity.java
+++ CStart/app/src/main/java/com/fat246/cstart/activities/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.fat246.cstart.R;
 import com.roughike.bottombar.BottomBar;
@@ -14,27 +12,37 @@ import com.roughike.bottombar.OnMenuTabClickListener;
 public class MainActivity extends AppCompatActivity {
 
     private BottomBar mBottomBar;
-    private TextView mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessage = (TextView) findViewById(R.id.message);
+        initBottomBar(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //保存BottomBar的状态
+        mBottomBar.onSaveInstanceState(outState);
+    }
+
+    //初始化BottomBar
+    private void initBottomBar(Bundle savedInstanceState) {
+
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItems(R.menu.menu_bottombar);
         mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId) {
 
-                mMessage.setText(menuItemId + "");
             }
 
             @Override
             public void onMenuTabReSelected(@IdRes int menuItemId) {
 
-                Toast.makeText(MainActivity.this, menuItemId + "", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -43,12 +51,5 @@ public class MainActivity extends AppCompatActivity {
         mBottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.colorPrimary));
         mBottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.colorPrimary));
         mBottomBar.mapColorForTab(4, ContextCompat.getColor(this, R.color.colorPrimary));
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        mBottomBar.onSaveInstanceState(outState);
     }
 }
