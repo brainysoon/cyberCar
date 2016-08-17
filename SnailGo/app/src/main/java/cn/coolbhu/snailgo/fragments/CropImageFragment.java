b922b8e0cc748a2a5c80c65215f08c151a7d10cb
kensoon <kensoon918@163.com>
2016-08-17 22:18:21 +0800

    fix-crop-image-permission-bug

--- SnailGo/app/src/main/java/cn/coolbhu/snailgo/fragments/CropImageFragment.java
+++ SnailGo/app/src/main/java/cn/coolbhu/snailgo/fragments/CropImageFragment.java
@@ -1,15 +1,20 @@
 package cn.coolbhu.snailgo.fragments;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +31,14 @@ import java.io.File;
 import cn.coolbhu.snailgo.MyApplication;
 import cn.coolbhu.snailgo.R;
 import cn.coolbhu.snailgo.activities.CropImageActivity;
+import cn.coolbhu.snailgo.utils.IntentUtils;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class CropImageFragment extends Fragment {
     private static final int REQUEST_PICK_IMAGE = 10011;
     private static final int REQUEST_SAF_PICK_IMAGE = 10012;
@@ -38,6 +50,8 @@ public class CropImageFragment extends Fragment {
     private Button mButtonCancle;
     private FloatingActionButton mActionOpen;
     private FloatingActionButton mActionPick;
+
+    private View rootView;
 
     public CropImageFragment() {
         // Required empty public constructor
@@ -71,6 +85,8 @@ public class CropImageFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        this.rootView = view;
+
         initRootView(view);
     }
 
@@ -80,15 +96,15 @@ public class CropImageFragment extends Fragment {
         findView(view);
 
         //setListener
-
-
         initCropImage();
 
-        setListener();
+        setListenerWithNoPermission();
+
+        CropImageFragmentPermissionsDispatcher.setListenerWithCheck(this);
     }
 
-    //setLesener
-    private void setListener() {
+    //Listener
+    private void setListenerWithNoPermission() {
 
         //取消
         mButtonCancle.setOnClickListener(new View.OnClickListener() {
@@ -96,24 +112,6 @@ public class CropImageFragment extends Fragment {
             public void onClick(View v) {
 
                 getActivity().finish();
-            }
-        });
-
-        //openFile
-        mActionOpen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //挑选图片
-                pickImage();
-            }
-        });
-
-        mActionPick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                pickPhoto();
             }
         });
 
@@ -155,11 +153,80 @@ public class CropImageFragment extends Fragment {
         });
     }
 
+    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void setListener() {
+
+        //openFile
+        mActionOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //挑选图片
+                pickImage();
+            }
+        });
+
+        mActionPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pickPhoto();
+            }
+        });
+
+    }
+
+    @OnShowRationale({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void showLoacationRationale(final PermissionRequest request) {
+
+        new AlertDialog.Builder(getContext())
+                .setTitle(R.string.request_permission)
+                .setIcon(R.mipmap.ic_launcher)
+                .setMessage(R.string.request_location_permission)
+                .setPositiveButton(R.string.allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.cancle, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        request.cancel();
+                    }
+                })
+
+                .setCancelable(false)
+                .show();
+    }
+
+    @OnPermissionDenied({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void showLoacationDenied() {
+
+        Snackbar.make(rootView, R.string.permission_denie, Snackbar.LENGTH_LONG)
+                .setAction(R.string.setting, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        IntentUtils.toSnailGoSettings(getContext());
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        CropImageFragmentPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
     //Uri
     public Uri createSaveUri() {
         return Uri.fromFile(new File(MyApplication.USER_AVATOR_DIRCTORY));
     }
-
 
     //initCropImage
     private void initCropImage() {
@@ -202,8 +269,6 @@ public class CropImageFragment extends Fragment {
             startActivityForResult(intent, REQUEST_SAF_PICK_IMAGE);
         }
     }
-
-    //
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent result) {
